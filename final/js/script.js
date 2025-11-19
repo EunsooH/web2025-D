0c1f934155c43515e4ef2bef48ebb4c67006f7a8
EunsooH <ehan@eastsideprep.org>
2025-11-19 09:35:14 -0800

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,12 @@
+function askQuestion() {
+    var p = prompt("what is your most favorite food?")
+    if (p !=null) {
+    document.getElementById ("question").innerHTML='NO '+ p + ".IT'S ALWAYS ICE CREAM WHAT ARE YOU TALKING ABOUT";
+    }
+}
+
+function MyName(){
+var p
+
+
+}
